624ccb2dcab0b32cc3c75e1010aa986eae27823f
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-03-23 15:18:14 -0300

    reverse

--- reveser.js
+++ reveser.js
@@ -5,3 +5,10 @@ console.log(reverse("hello"));
 
 // console.log("hello".substring(2));
 // console.log("hello".charAt(0));
+
+/*substring: char o
+substring: o char l
+substring: lo char l
+substring: llo char e
+substring: ello char h
+olleh*/

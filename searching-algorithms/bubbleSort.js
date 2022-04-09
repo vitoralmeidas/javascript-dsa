e94953faf977cad21a3d4ed338276ae9cf07396e
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-04-09 13:15:56 -0300

    another solution version Bubble Sort

--- searching-algorithms/bubbleSort.js
+++ searching-algorithms/bubbleSort.js
@@ -6,9 +6,9 @@
 */
 
 const bubbleSort = arr => {
-    for (let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length; j++){
-            console.log(arr);
+    for (let i = arr.length; i > 0; i--) {
+        for(let j = 0; j < i - 1; j++){
+            console.log(arr, arr[j], arr[j+1]);
             if (arr[j] > arr[j+1]) {
                 //Swap
                 let temp = arr[j];
@@ -17,11 +17,15 @@ const bubbleSort = arr => {
             }
         }        
     }
-
     return arr;
 }
 
-bubbleSort([37, 45, 29, 8])
-bubbleSort([15, 2, 9, 1])
-bubbleSort([3, 5, 29, 1])
-bubbleSort([1, 8, 27, 10])
+
+const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1];]
+}
+
+console.log(bubbleSort([37, 45, 29, 8]))
+// bubbleSort([15, 2, 9, 1])
+// bubbleSort([3, 5, 29, 1])
+// bubbleSort([1, 8, 27, 10])

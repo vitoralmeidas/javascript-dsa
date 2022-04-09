b03a44eb56ed27a34c8b6e6add6e6228cdf15796
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-04-09 12:56:11 -0300

    starting the studies about searching algo - Bubble Sort

---
+++ searching-algorithms/bubbleSort.js
@@ -0,0 +1,13 @@
+/*
+ - Starting looping from with a variable called i the endo of the array towardas the beginning
+ - Start an inner loop with a variable called j from the beginning until i - 1
+ - If arr[j] is greater than arr[j+1], swap those two values!
+ - Return the sorted array
+*/
+
+const bubbleSort = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        
+    }
+}

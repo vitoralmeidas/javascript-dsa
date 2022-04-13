3bba733570a6e915383490d3b8f2eecb12d2c3cb
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-13 14:40:38 -0300

    unshift -> ok

--- data-structure/SinglyLinkedList/SinglyLinkedList.js
+++ data-structure/SinglyLinkedList/SinglyLinkedList.js
@@ -79,6 +79,28 @@ class SinglyLinkedList {
     }
     return lastHead;
   }
+
+  /*
+  - This function should accept a value
+  - Create a new node using the value passed to the function
+  - If there is no head property on the list, set the head and tail to be the newly created node
+  - Otherwise set the newly created node's next property to be the current head property on the list
+  - Set the head property on the list to be that newly created node
+  - Increment the length of the list by 1
+  - Return the linked list
+  */
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();

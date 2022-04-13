949241835c533a4a3d605c0c0a2c7174932ed5cd
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-13 14:05:00 -0300

    pop() -> ok

--- data-structure/SinglyLinkedList/SinglyLinkedList.js
+++ data-structure/SinglyLinkedList/SinglyLinkedList.js
@@ -12,6 +12,17 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
+  /*
+    Method push()
+    - This function should accept a value/;
+    - Create an new node using the value passed to the function
+    - If there is no head property on the list, set the head and the tail to be the newly created node
+    - Otherwise set the next property o the tail to be the new node and set the tail property 
+        on the list to be the newly created node
+    - Increment the length by one
+    - return the list (this)
+ */
+
   push(val) {
     let newNode = new Node(val);
     if (!this.head) {
@@ -24,17 +35,40 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  /*
+    - If there are o nodes in the list, return undefined
+    - Loop throught the list until you reach the tail
+    - Set the next property of the 2nd to last node to be null
+    - Set the tail to be the 2nd to last node
+    - Decrement the length of the list by 1
+    - Return the value if the node removed
+  */
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
-let list = new SinglyLinkedList(0);
-console.log(list.push(10));
-/*
-    Method push()
-    - This function should accept a value/;
-    - Create an new node using the value passed to the function
-    - If there is no head property on the list, set the head and the tail to be the newly created node
-    - Otherwise set the next property o the tail to be the new node and set the tail property 
-        on the list to be the newly created node
-    - Increment the length by one
-    - return the list (this)
-    */
+let list = new SinglyLinkedList();
+// console.log(list.push(1));
+// console.log(list.push(2));
+// console.log(list.push(3));
+// console.log(list.push(4));
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
+console.log(list);

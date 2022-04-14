005bcc44c6b84bddc1c551f9ada94fc431b86653
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-14 13:19:07 -0300

    Linked List method -> set()

--- data-structure/SinglyLinkedList/SinglyLinkedList.js
+++ data-structure/SinglyLinkedList/SinglyLinkedList.js
@@ -120,6 +120,14 @@ class SinglyLinkedList {
     }
     return current;
   }
+  /*
+    - This function should accept a value and an index
+    - User your get function to find the specific node
+    - If the node is not found, return  false
+    - If the node is found, set the value of that node to be the value passed to the function and return true
+   */
+
+  set() {}
 }
 
 let list = new SinglyLinkedList();

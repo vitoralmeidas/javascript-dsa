2355d31ddb01c56069841a7f49f67f2b88d10854
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-14 11:10:36 -0300

    get() -> ok

--- data-structure/SinglyLinkedList/SinglyLinkedList.js
+++ data-structure/SinglyLinkedList/SinglyLinkedList.js
@@ -105,8 +105,21 @@ class SinglyLinkedList {
   }
 
   /*
-  
+    - This function shoudl accept an index
+    - If the index is less than zero or greater than or equal to the length of the list, return null
+    - Loop through the list until you reach the index and return the node at that specific index
   */
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();

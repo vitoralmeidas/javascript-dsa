ef2f84bb06b282aaaa62551cb0044da83c9a0614
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-14 14:20:34 -0300

    Linked List method -> remove()

--- data-structure/SinglyLinkedList/SinglyLinkedList.js
+++ data-structure/SinglyLinkedList/SinglyLinkedList.js
@@ -157,6 +157,17 @@ class SinglyLinkedList {
     this.length++;
     return true;
   }
+
+  /*
+  - If the index is less than zero or greater than the length, return undefined
+  - If the index is the same as the length-1, pop
+  - If the index is 0, shift
+  - Otherwise, using the get method, access the node at the index - 1
+  - Set the next property on that node to be the next of the next node
+  - Decrement the length
+  - Return the value of the node removed
+  */
+  remove(index) {}
 }
 
 let list = new SinglyLinkedList();

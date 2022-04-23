bdd8f6da23ab4ae3799d0854fbcf4afd706638f3
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-04-22 21:56:38 -0300

    HashTable set() -> ok

--- data-structure/HashTable/HashTable.js
+++ data-structure/HashTable/HashTable.js
@@ -2,14 +2,14 @@ class HashTable {
     constructor(size=53) {
         this.keyMap = new Array(size); // length Array
     }
-    
+
     _hash(key) {
         // the index of our key
         let total = 0;
         // to avoid colission
         let WEIRD_PRIMER = 31;  
-        // don't loop so much, max 100
-        for(let i = 0; i < Math.min(this.keyMap.length, 100); i++){ 
+        // don't loop so much, max 100 or the key length
+        for(let i = 0; i < Math.min(key.length, 100); i++){ 
             let char = key[i];
             // convert the letter in a number
             // map "a" to 1, "b" to 2, "c" to 3...
@@ -20,4 +20,21 @@ class HashTable {
         }
         return total;
     }
+
+    /*
+        SET 
+     - Accepts a key and a value
+     - Hashes the key
+     - Stores the key-value pair in the hash table array via separate chaining
+    */
+
+     set(key, value) {
+        let index = this._hash(key);
+        // check if there is something in index on keyMap
+        if(!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        // add in keyMap a new array where already exist something in that index
+        this.keyMap[index].push([key, value]);
+     }
 }

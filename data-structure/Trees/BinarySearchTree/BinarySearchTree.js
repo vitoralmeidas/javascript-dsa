99d46a3565c28a59c9285792c87802ff734d058d
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-29 09:23:19 -0300

    BST - method found()

--- data-structure/Trees/BinarySearchTree/BinarySearchTree.js
+++ data-structure/Trees/BinarySearchTree/BinarySearchTree.js
@@ -12,21 +12,21 @@ class BinarySearchTree {
   }
 
   /*
-            - Inserting
-         - Create a new node
-         - Starting at the root
-            - Check if there is a root, if not - the root now becomes that new node!
-            - If there is a root, check if the value of the new node is greater than
-                or less than the value of the root
-            - If it is greater
-                - Check to see if there is a node to the right
-                    - If there is, move to that node and repeat theses steps
-                    - If there is not, add that node as the right property 
-            -  If is is less
-                - Check to see if there is a node to the left
-                    - If there is, move to that node and repeat theses steps
-                    - If there is not, add that node as the left property
-        */
+        - Inserting
+    - Create a new node
+    - Starting at the root
+    - Check if there is a root, if not - the root now becomes that new node!
+    - If there is a root, check if the value of the new node is greater than
+        or less than the value of the root
+    - If it is greater
+        - Check to see if there is a node to the right
+            - If there is, move to that node and repeat theses steps
+            - If there is not, add that node as the right property 
+    -  If is is less
+        - Check to see if there is a node to the left
+            - If there is, move to that node and repeat theses steps
+            - If there is not, add that node as the left property
+*/
 
   insert(value) {
     let newNode = new Node(value);
@@ -52,6 +52,25 @@ class BinarySearchTree {
       }
     }
   }
+
+  /*
+        - Finding
+     - Starting at the root
+        - Check if there is a root, if not - we're done searching!
+        - If there is a root, check if the value of the new node is the value
+            we are looking for. If we found it, we're done!
+        - If not, check to see if the value is greater than or less than the value
+            of the root
+        - If it is greater
+            - Check to see of there is a node to the right
+                - If there is, move to that node and repeat theses steps
+                - If there is not, we're done searching!
+        - If it is less
+            - Check to see if there is a node to the left
+                - If there is, move to that node and repeat these steps
+                - If there is not, we're done searching!
+  */
+  found(value) {}
 }
 
 // let tree = new BinarySearchTree();

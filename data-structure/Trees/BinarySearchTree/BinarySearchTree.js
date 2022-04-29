a0b913e904a98c3d7274e6d40b623410816eeac0
Vitor Oliveira <vitor.oliveira1904@gmail.com>
2022-04-29 09:13:09 -0300

    BST - insert() -> ok

--- data-structure/Trees/BinarySearchTree/BinarySearchTree.js
+++ data-structure/Trees/BinarySearchTree/BinarySearchTree.js
@@ -2,7 +2,7 @@ class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
-    this.rigth = null;
+    this.right = null;
   }
 }
 
@@ -12,26 +12,46 @@ class BinarySearchTree {
   }
 
   /*
-        - Inserting
-     - Create a new node
-     - Starting at the root
-        - Check if there is a root, if not - the root now becomes that new node!
-        - If there is a root, check if the value of the new node is greater than
-            or less than the value of the root
-        - If it is greater
-            - Check to see if there is a node to the right
-                - If there is, move to that node and repeat theses steps
-                - If there is not, add that node as the right property 
-        -  If is is less
-            - Check to see if there is a node to the left
-                - If there is, move to that node and repeat theses steps
-                - If there is not, add that node as the left property
-    */
+            - Inserting
+         - Create a new node
+         - Starting at the root
+            - Check if there is a root, if not - the root now becomes that new node!
+            - If there is a root, check if the value of the new node is greater than
+                or less than the value of the root
+            - If it is greater
+                - Check to see if there is a node to the right
+                    - If there is, move to that node and repeat theses steps
+                    - If there is not, add that node as the right property 
+            -  If is is less
+                - Check to see if there is a node to the left
+                    - If there is, move to that node and repeat theses steps
+                    - If there is not, add that node as the left property
+        */
 
   insert(value) {
     let newNode = new Node(value);
-    if (!this.root) {
+    if (this.root === null) {
       this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
     }
   }
 }
+
+// let tree = new BinarySearchTree();

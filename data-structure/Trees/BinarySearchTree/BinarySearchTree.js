0720ae1e0dffc279d5df875a809fb0fc1b3766e2
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-05-01 22:18:50 -0300

    Depth First Search DFSInOrder() -> ok

--- data-structure/Trees/BinarySearchTree/BinarySearchTree.js
+++ data-structure/Trees/BinarySearchTree/BinarySearchTree.js
@@ -171,10 +171,26 @@ class BinarySearchTree {
 
   /*
       -DFSInOnder
-    -
+    - Create a variable to store the values of nodes visited
+    - Store the root of the BST in a variable called current
+    - Write a helper function which accepts a node
+      - If the node has a left property, call the helper function with the left property on the node
+      - Push the value of the node to the variable that stores the values
+      - If the node has a right property, call the helper function with the right property on the node
+      - Invoke the helper function with the current variable
+    - Return the array of values
   */
 
-  DFSInOrder() {}
+  DFSInOrder() {
+    let data = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return data;
+  }
 }
 
 // let tree = new BinarySearchTree();

b66e9b758341d8cdc9421c453cc5e7aafddd1630
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-06-03 18:06:33 -0300

    fix it

--- test.js
+++ test.js
@@ -1,21 +1,4 @@
+const arrayDiff = (a, b) => a.filter((e) => !b.includes(e));
 
-const maskify = (cc) =>{
-  if(cc.length <= 4) return cc;
-  let final = cc.length - 1;
-  let final2 = cc.slice(final - 3, final + 1)
-  let rest = cc.replace(final2, '').split('').map((num) => num = '#').join('')
-  return rest + final2;
-}
-
-console.log(maskify('64607935616'));
-
-const threeOrFive = (number) => {
-  let sum = 0;
-  for (let i = 0; i < number; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  }
-  return sum;
-};
-console.log(threeOrFive(10))
+console.log(arrayDiff([1, 2, 3], [1, 2])); // [1]
+console.log(arrayDiff([1, 2, 2, 2, 3], [2])); // [1,3]

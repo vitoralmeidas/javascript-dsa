023b325b275a1c674d5a2530e9a1496f2073ae06
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-06-29 19:21:00 -0300

    extractMax() ---> sinkDown()

--- data-structure/Heaps/MaxBinaryHeap.js
+++ data-structure/Heaps/MaxBinaryHeap.js
@@ -21,11 +21,52 @@ class MaxBinaryHeap {
     }
   }
 
-  // extractMax() {
-  //   return this.sinkDown()
-  // }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
 
-  // sinkDown({
+  sinkDown() {
+    while (true) {
+      let idx = 0;
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      // to grab the values, not indexes
+      // they're empty, cause we need to check if they'll exist or not
+      let rightChild, leftChild;
+      // it'll be a index, either left or right
+      let swap = null;
 
-  // })
+      //make sure its not out of bounds
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      //right -> make sure its not out of bounds
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      // first
+      this.values[idx] = this.values[swap];
+      // update the element when swap isn't null
+      this.values[swap] = element;
+      // update the index too
+      idx = swap;
+    }
+  }
 }

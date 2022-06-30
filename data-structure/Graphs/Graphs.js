ba2820f0832630630edf5e73ac3e7a8c5eb18c63
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-06-30 20:44:01 -0300

    Graphs methods (not error handling)

---
+++ data-structure/Graphs/Graphs.js
@@ -0,0 +1,14 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+}

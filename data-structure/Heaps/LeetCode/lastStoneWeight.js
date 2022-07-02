a0927c2c7a7e4f4ded5500572ed53aa795187a5b
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-01 23:31:17 -0300

    heaps -> problems

---
+++ data-structure/Heaps/LeetCode/lastStoneWeight.js
@@ -0,0 +1,10 @@
+var lastStoneWeight = function (stones) {
+  if (stones.length < 2) return stones;
+  stones.sort((a, b) => a - b);
+  let a = stones.pop();
+  let b = stones.pop();
+  stones.push(Math.abs(a - b));
+  return lastStoneWeight(stones);
+};
+
+console.log(lastStoneWeight([2, 7]));
